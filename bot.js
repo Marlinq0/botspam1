60929ab24b25d850a3d113d1536bc0567154410e
Marlinq0 <59901783+Marlinq0@users.noreply.github.com>
2020-03-12 03:43:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,56 @@
- const Discord = require('discord.js');
-const random = require('random');
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://lithiu.glitch.me/`);
+}, 280000);
+ var userTickets = new Map();
+// كل البكجات الي ممكن تحتجها في اي بوت
+const { Client, RichEmbed } = require("discord.js");
+var { Util } = require('discord.js');
+const {YT_API_KEY, prefix, devs} = require('./config')
+const client = new Client({ disableEveryone: true})
+const ytdl = require("ytdl-core");
+const canvas = require("canvas");
+const Canvas = require("canvas");
+const convert = require("hh-mm-ss")
+const fetchVideoInfo = require("youtube-info");
+const botversion = require('./package.json').version;
+const simpleytapi = require('simple-youtube-api')
+const moment = require("moment");
+const fs = require('fs');
+const util = require("util")
+const gif = require("gif-search");
+const opus = require("node-opus");
+const ms = require("ms");
+const jimp = require("jimp");
+const { get } = require('snekfetch');
+const guild = require('guild');
+const db = require("quick.db")
 const dateFormat = require('dateformat');//npm i dateformat
+const YouTube = require('simple-youtube-api');
+const youtube = new YouTube('AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8');
+const hastebins = require('hastebin-gen');
+const getYoutubeID = require('get-youtube-id');
+const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
+const pretty = require("pretty-ms");
+client.login(process.env.TOKEN);
+const queue = new Map();
+var table = require('table').table
+const Discord = require('discord.js');
+const premium = ["470673278709530634"];
+const random = require('random');
 const jsonfile = require('jsonfile');
-const client = new Discord.Client();
-console.log("Scrpit By Dream");
-client.login(process.env.BOT_TOKEN); 
+
+
+
+
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-
   if(client.user.in){
    
    }
@@ -721,14 +763,4 @@ client.on('messageReactionAdd', (reaction, user) => {
         }
     }
 });
-
-
-client.on('ready',async () => {
-console.log("Conneted to room..");
-let g = client.guilds.get("211543198651121664"); // id server
-let c = g.channels.get("684040797561290753");// id channel
-if(c.type === 'voice') {
-c.join();
-}});
-
 
